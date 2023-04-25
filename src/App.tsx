7eb827a8dcd15a16af880c33b06ddb422fee9111
QDLu <quangdunglu159@gmail.com>
2023-04-25 20:14:47 +0700

    Add lib

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,11 @@ import "./App.css";
 
 import { useSelector } from "react-redux";
 import { Navigate } from "react-router";
-import { Login } from "./pages/account";
+import { Login, Register } from "./pages/account";
 import { Admin } from "./pages/admin/Admin";
 import { AppState } from "./store";
 import { AccountState } from "./store/account/types";
-import "./styles/sb-admin-2.min.css";
+import "./styles/sb-admin-2.css";
 
 function App() {
   const account: AccountState = useSelector((state: AppState) => state.account);
@@ -16,14 +16,18 @@ function App() {
       <Router>
         <Routes>
           <Route
-            element={
-              account.token ? <Navigate to="/" replace /> : <Login />
-            }
+            element={account.token ? <Navigate to="/" replace /> : <Login />}
             path="/login"
           />
           <Route
-            element={account.token ? <Admin /> : <Navigate to="/login" replace />}
+            element={
+              account.token ? <Admin /> : <Navigate to="/login" replace />
+            }
             path="/"
+          />
+          <Route
+            element={account.token ? <Navigate to="/" replace /> : <Register />}
+            path="/register"
           />
         </Routes>
       </Router>
